1a59eae9d824b873f4ec5b960c8a45fee158ef1c
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-09-30 23:33:10 -0300

    comcatenação

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-/*
-Ex1:
+
+//Ex1:
 alert('Boas vinda ao jogo do número secreto');
 let numeroSecreto = 5;
+console.log(numeroSecreto);
 let chute = prompt('Esccolha um número entre 1 e 10');
 
+//se chute for igual ao número secreto
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)')
+    alert(`Isso ai! Você descobriu o número secreto  ${numeroSecreto}`);
+} else {
+    alert('Vovê errou :(');
 }
 
+/*
 Ex2:    
 alert('Boas vindas ao nosso site!');
 let nome = 'Lua';
@@ -18,8 +23,7 @@ alert("Erro!Preencha todos os campos");
 let mensagemDeErro = "Erro! Preencher todos os campos";
 alert(mensagemDeErro);
 
-*/
-
+Ex3:
 let nome = prompt("Qaul o nome do usuario?");
 let idade = prompt("Qual a sua idade?");
 
@@ -28,3 +32,7 @@ if(idade >= 18){
 }else{
     alert("Não pode dirigir!");
 }
+
+*/
+
+ //

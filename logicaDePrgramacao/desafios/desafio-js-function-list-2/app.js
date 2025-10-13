526dc950c43f4821c11a52994c85124209815b36
Rodrigo G Pereira <rodrigogr.pereira@gmail.com>
2025-10-12 21:18:15 -0300

    Desafio 2 part 2

--- logicaDePrgramacao/desafios/desafio-js-function-list-2/app.js
+++ logicaDePrgramacao/desafios/desafio-js-function-list-2/app.js
@@ -82,3 +82,26 @@
 
     // Exemplo de uso:
     mostrarTabuada(7);
+
+    //Crie uma lista vazia, com o nome listaGenerica
+    let listaGenerica = [];
+
+    //Crie uma lista de linguagens de programação chamada linguagensDeProgramacao
+    let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+
+    //Adicione à lista linguagensDeProgramacao os seguintes elementos: Java, Ruby e GoLang.
+    let linguagensDeProgramacao2 = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+    linguagensDeProgramacao2.push('Java', 'Ruby', 'GoLang');
+    // Lista ao final: ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+
+    //Crie uma lista com 3 nomes e exiba no console apenas o primeiro elemento.
+    nomes = ['JavaScript', 'Python', 'Go'];
+    console.log(nomes[0]);
+
+    //Crie uma lista com 3 nomes e exiba no console apenas o segundo elemento.
+    nomes = ['JavaScript', 'Python', 'Go'];
+    console.log(nomes[1]);
+
+    //Crie uma lista com 3 nomes e exiba no console apenas o último elemento.
+    nomes = ['JavaScript', 'Python', 'Go'];
+    console.log(nomes[2]);
--- logicaDePrgramacao/funcoes-listas-js/app.js
+++ logicaDePrgramacao/funcoes-listas-js/app.js
@@ -3,9 +3,11 @@
 
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
-
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto =  gerarNumeroAleatorio();
 let tentativas = 1;
+
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -40,7 +42,19 @@ function verificarChute(){
 }
 
 function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1);
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
 }
 
 function limparCampo(){
